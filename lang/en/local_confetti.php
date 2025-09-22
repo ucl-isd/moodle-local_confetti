9fdabe87af297977a063c59a8c84abf8e780d51d
Matthias Opitz <opitz@gmx.de>
2025-09-22 16:53:09 +0100

    fixed PHPCS issues

--- lang/en/local_confetti.php
+++ lang/en/local_confetti.php
@@ -1,4 +1,28 @@
 <?php
-$string['pluginname'] = 'Confetti';
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for local_confetti
+ *
+ * @package    local_confetti
+ * @copyright  2025 UCL <m.opitz@ucl.ac.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $string['classes'] = 'Target classes';
 $string['classes_desc'] = 'Comma-separated list of CSS classes that should trigger confetti when clicked.';
+
+$string['pluginname'] = 'Confetti';
